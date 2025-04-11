7fdba25a37f15af7811ca855b70982098fcd1f60
calic-dev <82257339+calic-dev@users.noreply.github.com>
2025-04-12 00:45:29 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// ostatak tvoje aplikacije
 import { useState, useEffect } from 'react'
 import { Card, CardContent } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
